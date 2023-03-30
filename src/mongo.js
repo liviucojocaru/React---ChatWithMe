dd3a185e03b660e4a7fbe917f4abb0d8d0eaa24e
liviucojocaru <liviucojocaru@outlook.com>
2023-03-31 01:02:49 +1300

    next

--- src/mongo.js
+++ src/mongo.js
@@ -1,22 +1,27 @@
+require("dotenv").config();
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
+console.log("Connecting to MongoDB Atlas...");
+console.log("URI:", uri);
+
+client.connect((err) => {
   if (err) {
-    console.log(err);
+    console.log("Error connecting to MongoDB Atlas:", err);
     return;
   }
   console.log("Connected to MongoDB Atlas");
   const db = client.db();
   db.admin().listDatabases((err, result) => {
     if (err) {
-      console.log(err);
+      console.log("Error listing databases:", err);
       return;
     }
     console.log("Databases:");
-    console.log(result.databases);
+    console.table(result.databases);
     client.close();
+    console.log("Connection closed");
   });
 });
