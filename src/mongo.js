59dfab1998c752818d836e6c30543b725b10995e
liviucojocaru <liviucojocaru@outlook.com>
2023-03-31 00:23:28 +1300

    Next steps

---
+++ src/mongo.js
@@ -0,0 +1,21 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connected to MongoDB Atlas");
+  const db = client.db();
+  db.admin().listDatabases((err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Databases:");
+    console.log(result.databases);
+    client.close();
+  });
+});
